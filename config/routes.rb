907b52240074e3528bd9376d317f2be1d5928ba9
Jan Wieczorkowski <jan.wieczorkowski@naturaily.com>
2016-08-22 13:47:08 +0200

    Skip unnecessary doorkeeper controllers.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  use_doorkeeper
+  use_doorkeeper do
+    skip_controllers :authorizations, :applications, :authorized_applications
+  end
+
   devise_for :users
 
   resources :items
